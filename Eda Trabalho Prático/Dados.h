71420e0dee629988f198f53c68c96b96533df50d
Joninho Rafinha <92312858+JoninhoRafinha@users.noreply.github.com>
2022-06-01 20:47:31 +0100

    Add files via upload

---
+++ Eda Trabalho Prático/Dados.h
@@ -0,0 +1,67 @@
+﻿/**
+ * Author: Jo�o Antune
+ * Email: joaorantune03@gmail.com
+ * Github: JoninhoRafinha
+ * Date: 27/03/2022
+ * Descri��o: Assinatura de Fun��es e Structs para o Structs.c do Trabalho Pr�trico - EDA
+ */
+
+#ifndef Structs
+#define Structs
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma warning( disable : 4996 )
+
+#pragma region Estruturas
+
+ /**
+  * @brief Estrutura para armazenar uma Operacao
+  *
+  * Uma Operacao cont�m um Id (@@Id) e uma Lista de Maquinas (@@Maquinas).
+  * Cont�m apontador para a proxima Operacao
+  */
+typedef struct Operacao
+{
+    int Id;
+    struct Maquina* Maquinas;
+    struct Operacao* Next;
+} Operacao;
+
+/**
+ * @brief Estrutura para armazenar uma Maquina
+ *
+ * Uma Maquina cont�m um Id (@@Id) e um Tempo (@@Tempo).
+ * Cont�m apontador para a proxima Maquina
+ */
+typedef struct Maquina
+{
+    int Id;
+    int Tempo;
+    struct Maquina* Next;
+} Maquina;
+
+/**
+ * @brief Estrutura para armazenar um Job
+ *
+ * Um Job cont�m um Id (@@Id) e uma Lista de Operacoes (@@Operacoes).
+ * Cont�m apontador para o proximo Job
+ */
+typedef struct Job
+{
+    int Id;
+    struct Operacao* Operacoes;
+    struct Job* Next;
+} Job;
+
+#pragma endregion
+
+#pragma region Ficheiros
+
+Job* LerFicheiro();
+
+#pragma endregion
+
+#endif
---
+++ Eda Trabalho Prático/Stucts.c
@@ -0,0 +1,22 @@
+﻿/**
+ * Author: Jo�o Antunes
+ * Email: joaorantune03@gmail.com
+ * Github: JoninhoRafinha
+ * Date: 27/03/2022
+ * Descri��o: Fun��es das Operacoes, Trabalho Pratico - EDA
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Dados.h"
+
+#pragma region Ficheiros
+
+Job* LerFicheiro()
+{
+	FILE* Ficheiro;
+
+}
+
+#pragma endregion
